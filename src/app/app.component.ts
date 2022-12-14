ef5ec4c795c8e0883df37258136a1aba1f176a4a
Raúl Alexis <raul.ffxiii@gmail.com>
2022-12-14 00:47:29 -0600

    Actualizacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 interface Tarjeta{
   titulo: string;
   subtitulo: string;
-  nro?: number;
+  image: string;
 }
 
 @Component({
@@ -17,9 +17,9 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.Arreglotarjetas = [
-      {titulo: 'Video 1', subtitulo: 'Subtitulo video 1'},
-      {titulo: 'Video 2', subtitulo: 'Subtitulo video 2'},
-      {titulo: 'Video 3', subtitulo: 'Subtitulo video 3'},
+      {titulo: 'Video 1', subtitulo: 'Subtitulo video 1', image:"https://cdn.akamai.steamstatic.com/steam/apps/1599340/header.jpg?t=1670270556"},
+      {titulo: 'Video 2', subtitulo: 'Subtitulo video 2', image:"https://images.ctfassets.net/umhrp0op95v1/ucU2LmeKQdZDv8uqt8GKz/69817657f86042edef6a86987045b2cc/LA_STATIC_OPENGRAPH_600x300.jpg"},
+      {titulo: 'Video 3', subtitulo: 'Subtitulo video 3', image:"https://images.ctfassets.net/umhrp0op95v1/1ZN0phT6n06PovBRpQztUS/d37886447ee07f23890619bcc777df3a/LA_ALL_CLASSES_740x420.jpg"},
     ]
   }
 }
